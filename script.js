46a661f9cd371785655e302a19dd7120d784841c
nicolasalouf <alouf.nicolas@gmail.com>
2020-05-28 16:33:51 +0300

    page prompts for number of rows and script.js rearranged for clarity

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 
 // FUNCTIONS
+function createBox(n){
+    for (i=0; i<n*n; i++){
+        let div = document.createElement("div");
+        div.classList.add("box");
+        cont.appendChild(div);
+    }
+}
 
+function formatBox(width, height){
+    let box = document.querySelectorAll(".box");
+    box.forEach(box => box.setAttribute(    
+    "style","border: solid 0.05px; width:"+width+"px; height:"+height+"px; float: right"
+    ))
+}
+
+function drawGrid(){
+    let n = prompt("please enter n");
+    let width = (cont.clientWidth / n)-2;
+    let height = (cont.clientHeight / n)-2;
+    createBox(n);
+    formatBox(width, height);
+    }
 
 // VARIABLES
 const cont = document.querySelector("container");
@@ -8,16 +29,4 @@ const btn = document.querySelector("#btn");
 
 
 // COMMANDS
-btn.addEventListener("click", function(){
-    let n = 16
-    let width = (cont.clientWidth / n)-2;
-    let height = (cont.clientHeight / n)-2;
-    for (i=0; i<n*n; i++){
-        let div = document.createElement("div");
-        div.classList.add("box");
-        cont.appendChild(div);
-    }
-    let box = document.querySelectorAll(".box");
-    box.forEach(box => box.setAttribute(    
-    "style","border: solid 0.05px; width:"+width+"px; height:"+height+"px; float: right"
-    ))});
+btn.addEventListener("click", drawGrid);
