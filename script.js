ff8f49f2e6e503d53a2a4b70d64c222485f6627f
nicolasalouf <alouf.nicolas@gmail.com>
2020-05-28 16:20:58 +0300

    fixed size of inside boxes to fit container

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ const btn = document.querySelector("#btn");
 // COMMANDS
 btn.addEventListener("click", function(){
     let n = 16
-    let width = cont.clientWidth / n;
-    let height = cont.clientHeight / n;
-    for (i=0; i<n-1; i++){
+    let width = (cont.clientWidth / n)-2;
+    let height = (cont.clientHeight / n)-2;
+    for (i=0; i<n*n; i++){
         let div = document.createElement("div");
         div.classList.add("box");
         cont.appendChild(div);
     }
     let box = document.querySelectorAll(".box");
     box.forEach(box => box.setAttribute(    
-    "style","border: solid; width:"+width+"px; height:"+height+"px"
+    "style","border: solid 0.05px; width:"+width+"px; height:"+height+"px; float: right"
     ))});
